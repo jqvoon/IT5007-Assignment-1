e34274219c9e274b1d7326aa9502620f317e3713
JunQuan Voon <voonjunquan@gmail.com>
2026-02-15 14:25:57 +0800

    Set up the required for test scripts

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -7,5 +7,10 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
-  }
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/tests/setup.js',
+  },
 })
